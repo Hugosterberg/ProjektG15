f78235581b8ebd066cacf88869d82ae63dd9bab9
Hugosterberg <59054135+Hugosterberg@users.noreply.github.com>
2020-01-13 13:35:50 +0100

    Add project files.

---
+++ ProjektG15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjektG15.MvcApplication" Language="C#" %>
---
+++ ProjektG15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektG15.Startup))]
+namespace ProjektG15
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
